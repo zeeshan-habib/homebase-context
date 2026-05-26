c70674777237f9122931349cb0c32d39797568f8
zeeshan-habib <107956888+zeeshan-habib@users.noreply.github.com>
2026-05-25 20:27:41 -0400

    Fix missing matplotlib.patches import

--- data/product-areas/mshr/workflows/event-impact-template.py
+++ data/product-areas/mshr/workflows/event-impact-template.py
@@ -26,6 +26,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
+import matplotlib.patches as mpatches
 from scipy import stats
 
 # ─────────────────────────────────────────────────────────────────────────────
